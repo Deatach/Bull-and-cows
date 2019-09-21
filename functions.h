ccad6935db38c689949a700f2c3c053b19b1a331
Mironov Nikita <Hooclo@yandex.ru>
2019-09-21 17:07:03 +0700

    first

---
+++ functions.h
@@ -0,0 +1,22 @@
+#include <math.h>
+#include <graphics.h>
+#include <time.h>
+
+int q;
+
+void generate()
+{
+    q=(rand()%9000+1000);
+    qch[0]=q/1000;
+    qch[1]=q/100-qch[0]*10;
+    qch[2]=q/10-qch[1]*10-qch[0]*100;
+    qch[3]=q-10*qch[2]-100*qch[1]-1000*qch[0];
+    if ((qch[0]==qch[1])or(qch[0]==qch[2])or(qch[0]==qch[3])or(qch[1]==qch[2])or(qch[1]==qch[3])or(qch[2]==qch[3]))
+    
+    return generate();
+    
+    else
+    
+    return;
+}
+
