e955ae3309cf3ef96ea119bb71aadd2a3238c445
Mironov Nikita <Hooclo@yandex.ru>
2019-09-22 18:27:57 +0700

    add menu 3

--- BAC.cpp
+++ BAC.cpp
@@ -12,7 +12,34 @@ int menu()
     outtextxy(260, 360, "2 -  authors");
     outtextxy(260, 390, "3 -  exit");
 }
-
+    code = getch();
+    
+    if ( code == 49 ) 
+    {
+	generate();	
+	printch();
+	guess();
+    }
+    
+    if ( code == 50 ) 
+    {
+	bar (0, 0, 599, 599);
+	auth();
+	delay(3000);
+	return menu();
+    }
+    
+    if ( code == 51 ) 
+    {
+	return 0;
+    }
+    
+    
+    if ( (code != 51) and (code != 50) and (code !=49) ) 
+    {
+	return menu();
+    }
+}
 
 main()
 {
