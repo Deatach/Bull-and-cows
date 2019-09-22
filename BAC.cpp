477548e1fa87519cdef9609404738ddf7cbf9d35
Mironov Nikita <Hooclo@yandex.ru>
2019-09-22 18:17:59 +0700

    add menu

--- BAC.cpp
+++ BAC.cpp
@@ -1,6 +1,18 @@
 #include <math.h>
 #include <graphics.h>
 #include <time.h>
+#include "gameplay.h"
+
+int menu()
+{
+    setfillstyle(SOLID_FILL, BLACK);
+    bar (0, 0, 599, 599);
+    outtextxy(250, 300, "byky i korovi");
+    outtextxy(250, 320, "1 -  start game");
+    outtextxy(250, 350, "2 -  authors");
+    outtextxy(250, 400, "3 -  exit");
+}
+
 
 main()
 {
