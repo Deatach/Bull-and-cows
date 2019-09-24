88f1d826f12100d6d297659bfab845eb27f1d4c3
Evgen <cool.evp2000@yandex.ru>
2019-09-24 16:14:24 +0700

    Exit to menu

--- gameplay.h
+++ gameplay.h
@@ -3,6 +3,8 @@
 #include <time.h>
 #include "functions.h"
 
+int p1,p2;
+
 int guess()
 {
 setfillstyle(SOLID_FILL, GREEN);
@@ -10,4 +12,7 @@ bar (0, 0, 599, 599);
 srand(time(NULL));
 outtextxy(260, 150, "esc = return to menu");
 outtextxy(260, 300, "INSERT THE KEY");
+ach[0]=getch()-48;
+if (ach[0]==-21)
+return menu();
 }
