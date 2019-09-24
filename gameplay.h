1826157d08bed978a5a64cf2935d74c7a18e8ae9
Evgen <cool.evp2000@yandex.ru>
2019-09-24 16:11:14 +0700

    Start guess

--- gameplay.h
+++ gameplay.h
@@ -2,3 +2,12 @@
 #include <graphics.h>
 #include <time.h>
 #include "functions.h"
+
+int guess()
+{
+setfillstyle(SOLID_FILL, GREEN);
+bar (0, 0, 599, 599);
+srand(time(NULL));
+outtextxy(260, 150, "esc = return to menu");
+outtextxy(260, 300, "INSERT THE KEY");
+}
