3b2f91bfc080e66d6eba61363f649b0b0d79f34e
Evgen <cool.evp2000@yandex.ru>
2019-09-24 16:20:09 +0700

    Amount of right correct keys

--- gameplay.h
+++ gameplay.h
@@ -45,4 +45,12 @@ char f5[4];
 sprintf(f5, "%d", p1);
 outtextxy(220, 380, "CORRECT KEYS:");
 outtextxy(335, 380, f5);
+if(qch[0]==ach[0]) p2=p2+1;
+if(qch[1]==ach[1]) p2=p2+1;
+if(qch[2]==ach[2]) p2=p2+1;
+if(qch[3]==ach[3]) p2=p2+1;
+char f6[4];
+sprintf(f6, "%d", p2);
+outtextxy(147, 400, "CORRECT KEYS ON RIGHT PLACES:");
+outtextxy(390, 400, f6);
 }
