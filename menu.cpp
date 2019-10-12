012c614f1affbb5e67e5a59983e1007d51ccd27d
Evgen <cool.evp2000@yandex.ru>
2019-10-12 15:10:32 +0700

    separate_menu_2_good

---
+++ menu.cpp
@@ -0,0 +1,41 @@
+#include "gameplay.h"
+
+int menu()
+{
+{
+    setfillstyle(SOLID_FILL, BLACK);
+    bar (0, 0, 599, 599);
+    outtextxy(260, 300, "byky i korovi");
+    outtextxy(260, 330, "1 -  start game");
+    outtextxy(260, 360, "2 -  authors");
+    outtextxy(260, 390, "3 -  exit");
+}
+    code = getch();
+    
+    if ( code == 49 ) 
+    {
+	generate();	
+	printch();
+	guess();
+    }
+    
+    if ( code == 50 ) 
+    {
+	bar (0, 0, 599, 599);
+	auth();
+	delay(3000);
+	return menu();
+    }
+    
+    if ( code == 51 ) 
+    {
+	return 0;
+    }
+    
+    
+    if ( (code != 51) and (code != 50) and (code !=49) ) 
+    {
+	return menu();
+    }
+}
+
