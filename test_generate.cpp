8c94755dc0631e0cefd3bc74087d04b5a2fdff49
Mironov Nikita <Hooclo@yandex.ru>
2019-10-13 23:41:02 +0700

    tests auth

---
+++ test_generate.cpp
@@ -0,0 +1,45 @@
+#include "menu cpp"
+
+int men()
+{
+{
+    setfillstyle(SOLID_FILL, BLACK);
+    bar (0, 0, 599, 599);
+    outtextxy(260, 300, "byky i korovi");
+    outtextxy(260, 330, "1 -  start game");
+    outtextxy(260, 360, "2 -  authors");
+    outtextxy(260, 390, "3 -  exit");
+}
+    
+    if ( code == 49 ) 
+    {
+    generate();	
+    printch();
+    guess();
+    }
+    
+    if ( code == 50 ) 
+    {
+    Erase();
+    auth();
+    delay(3000)
+    }
+    
+    if ( code == 51 ) 
+    {
+    return 0;
+    }
+    
+    
+    if ( (code != 51) and (code != 50) and (code !=49) ) 
+    {
+    return men();
+    }
+}
+
+main()
+{
+    initwindow(600, 600);
+    code = 50;
+    men();
+}
