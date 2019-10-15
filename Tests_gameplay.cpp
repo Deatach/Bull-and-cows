e9c881da018add1f63511977781e7beec7a641ec
Evgen <cool.evp2000@yandex.ru>
2019-10-15 16:26:55 +0700

    cor_incor_test

--- Tests_gameplay.cpp
+++ Tests_gameplay.cpp
@@ -1,5 +1,8 @@
 #include "functions.h"
 
+int i,j,p1,p2,code;
+char f1[4],f2[4],f3[4],f4[4],f5[4];
+
 void Test_numbers()
 {
 	setfillstyle(SOLID_FILL, GREEN);
@@ -25,9 +28,24 @@ void Test_numbers()
 	delay(2500);	
 }
 
+void Test_correct_incorrect()
+{
+	outtextxy(220, 380, "CORRECT KEYS:");
+	outtextxy(335, 380, f5);
+	if(qch[0]==ach[0]) p2=p2+1;
+	if(qch[1]==ach[1]) p2=p2+1;
+	if(qch[2]==ach[2]) p2=p2+1;
+	if(qch[3]==ach[3]) p2=p2+1;
+	char f6[4];
+	sprintf(f6, "%d", p2);
+	outtextxy(147, 400, "CORRECT KEYS ON RIGHT PLACES:");
+	outtextxy(390, 400, f6);
+}
+
 main()
 {
 	srand(time(NULL));
 	initwindow(600, 600);
 	Test_numbers();
+	Test_correct_incorrect();
 }
