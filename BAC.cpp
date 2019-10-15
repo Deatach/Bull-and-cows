f6aa2a520ad5be91031e47e14a364a8b13e2bcfd
Evgen <cool.evp2000@yandex.ru>
2019-10-15 15:20:11 +0700

    Tests_0.2

--- BAC.cpp
+++ BAC.cpp
@@ -1,47 +1,8 @@
-#include "gameplay.h"
-
-int menu()
-{
-{
-	srand(time(NULL));
-    setfillstyle(SOLID_FILL, BLACK);
-    bar (0, 0, 599, 599);
-    outtextxy(260, 300, "byky i korovi");
-    outtextxy(260, 330, "1 -  start game");
-    outtextxy(260, 360, "2 -  authors");
-    outtextxy(260, 390, "3 -  exit");
-}
-    code = getch();
-    
-    if ( code == 49 ) 
-    {
-	generate();	
-	printch();
-	guess();
-    }
-    
-    if ( code == 50 ) 
-    {
-	bar (0, 0, 599, 599);
-	auth();
-	delay(3000);
-	return menu();
-    }
-    12
-    if ( code == 51 ) 
-    {
-	return 0;
-    }
-    
-    
-    if ( (code != 51) and (code != 50) and (code !=49) ) 
-    {
-	return menu();
-    }
-}
+#include "menu.cpp"
 
 main()
 {
+	srand(time(NULL));
     initwindow(600, 600);
     menu();
 }
